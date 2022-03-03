58f3d516bfb63d05b75feed80517078cd4d3246e
asiedusolo <solomon.asiedu@amalitech.org>
2022-03-03 16:32:11 +0000

    chat client files added

---
+++ src/pages/login/loginAPICalls.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const LoginCall = async (userCredentials, dispatch) => {
+  dispatch({ type: "LOGIN_START" });
+  try {
+    const response = await axios.post(
+      "https://letxchatapi.herokuapp.com/api/auth/login",
+      userCredentials
+    );
+    dispatch({ type: "LOGIN_SUCCESS", payload: response.data });
+  } catch (error) {
+    dispatch({ type: "LOGIN_FAILURE", payload: error });
+  }
+};
+
+export default LoginCall;
